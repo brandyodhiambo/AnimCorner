5310d6ff1e40e1ed32dfdc1235e4724db2c0ff12
brandy-kay <brandyariana99@gmail.com>
2022-06-14 06:13:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/animconer/presentation/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.animconer.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+//project colors
+val Pink = Color(0xFFD94061)
+val LightPurple = Color(0xFFAb84f9)
+val Brown = Color(0xFFB4a077)
+val LightBlue = Color(0xFF51c4ef)
+val DarkGray = Color(0xFF9d9d9f)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val BlueBlack = Color(0xFF1d2029)
+
+
+
